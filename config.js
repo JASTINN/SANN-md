bbd1a8adb884aab28b9c20238e2c226805c8f7ca
JASTINN <117553797+JASTINN@users.noreply.github.com>
2022-11-06 20:13:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
+  ['6282130340117'],
+  ['6282130340117'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
